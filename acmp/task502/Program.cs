b7d6b6c6c6805bae1dd7ff0bd4d22a8f9c0a246e
Alex <alvi88@list.ru>
2016-04-25 16:50:14 +0300

    Добавил заготовку для задачи с Лягушкой (№ 502)

---
+++ acmp/task502/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace task502
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("INPUT.TXT");
+            int n = int.Parse(sr.ReadLine());
+            int[,] a = new int[n,n];
+            string[] s;
+            for (int i = 0; i < n; ++i)
+            { 
+                s = sr.ReadLine().Split();
+                for (int j = 0; j < n; ++j)
+                    a[i,j] = int.Parse(s[j]);
+            }
+            sr.Close();
+            int[] cols = { };
+            int res = GetMosCount(n, a, n, cols);
+            StreamWriter sw = new StreamWriter("OUTPUT.TXT");
+            sw.Write(res);
+            sw.Close();
+        }
+
+        static int GetMosCount(int n, int[,] a, int rows, int[] cols)
+        {
+            return 0;
+        }
+    }
+}
