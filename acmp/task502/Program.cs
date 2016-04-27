95a5edd0875f2192c52f61357314f94022331251
Alex <alvi88@list.ru>
2016-04-27 16:50:04 +0300

    Try to do 502 task

--- acmp/task502/Program.cs
+++ acmp/task502/Program.cs
@@ -6,11 +6,14 @@ namespace task502
 {
     class Program
     {
+        static int  n;
+        static int[,] a;
+        static List<int> sumAr = new List<int>();
         static void Main(string[] args)
         {
             StreamReader sr = new StreamReader("INPUT.TXT");
-            int n = int.Parse(sr.ReadLine());
-            int[,] a = new int[n,n];
+            n = int.Parse(sr.ReadLine());
+            a = new int[n,n];
             string[] s;
             for (int i = 0; i < n; ++i)
             { 
@@ -19,16 +22,60 @@ namespace task502
                     a[i,j] = int.Parse(s[j]);
             }
             sr.Close();
-            int[] cols = { };
-            int res = GetMosCount(n, a, n, cols);
+            List<Pair> path = new List<Pair>();
+            GetMosCount(n, path);
+            int max = -1;
+            for (int i = 0; i < sumAr.Count; ++i)
+                if (sumAr[i] > max) max = sumAr[i];
             StreamWriter sw = new StreamWriter("OUTPUT.TXT");
-            sw.Write(res);
+            sw.Write(max);
             sw.Close();
         }
 
-        static int GetMosCount(int n, int[,] a, int rows, int[] cols)
+        struct Pair
         {
+            public int r, c;
+        }
+
+        static int GetMosCount(int rows, List<Pair> path)
+        {
+            int i, j, tmp;
+            if (rows == 1) //Попали в ветку где надо взять еще один элемент из 1 строки. Берем максимальный из оставшихся
+            {
+                tmp = -1;
+                for (j = 0; j < n; ++j)
+                    if ((!InPath(j, path)) && (a[0,j] > tmp)) tmp = a[0,j];
+                if (rows == n)
+                    sumAr.Add(tmp);
+                return tmp;
+            }
+            for (j = 0; j < n; ++j)
+            {
+                if (!InPath(j, path))
+                {
+                    for (i = 0; i < rows; ++i)
+                        if (rows - (i + 1) > 0)
+                        {
+                            List<Pair> pathn = new List<Pair>(path); pathn.Add(new Pair() { r = i, c = j });
+                            tmp = (GetMosCount(rows - (i + 1), pathn) + a[i, j]);
+                            if (rows == n)
+                                sumAr.Add(tmp);
+                        }
+                        else if (rows - (i + 1) == 0)
+                            return a[i,j];
+                }
+            }
             return 0;
+        }
+
+        static bool InPath(int j, List<Pair> path)
+        {
+            for (int k = 0; k < path.Count; ++k)
+            {
+                if (path[k].c == j)
+                    return true;
+            }
+            return false;
         }
     }
 }
