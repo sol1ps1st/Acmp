ee2b556a72f43927740321d9e1ceef5e3b535375
Alex <alvi88@list.ru>
2016-05-10 15:32:04 +0300

    add 519 task

---
+++ acmp/task519/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace task519
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            int n = int.Parse(sr.ReadLine());
+            sr.Close();
+            int min, max;
+            MinMax(n, out min, out max);
+            StreamWriter sw = new StreamWriter("output.txt");
+            sw.Write(min.ToString() + " " + max.ToString());
+            sw.Close();
+        }
+
+        static void MinMax(int n, out int min, out int max)
+        {
+            List<int> digits = new List<int>();
+            while (n > 0)
+            {
+                digits.Add(n - (n / 10) * 10);
+                n /= 10;
+            }
+
+            min = max = 0;
+            digits.Sort();
+            int i;
+            for (i = digits.Count-1; i >= 0; --i)
+                max += (digits[i] * (int)Math.Pow(10, i));
+
+            int firstNotNullEl = 0;// Не можем начать число с 0. Соответственно, ищем первый ненулевой элемент и ставим его на 1 место
+            while ((digits[firstNotNullEl] == 0) && (firstNotNullEl < digits.Count))
+                firstNotNullEl++;
+
+            if (firstNotNullEl != 0)
+            {
+                int tmp = digits[firstNotNullEl];
+                digits[firstNotNullEl] = 0;
+                digits[0] = tmp;
+            }
+            for (i = 0; i < digits.Count; ++i)
+                min += (digits[i] * (int)Math.Pow(10, digits.Count - 1 - i));
+        }
+    }
+}
---
+++ acmp/task519/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("task519")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task519")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("76d3b2a7-028a-4274-a2ce-0885d69b244e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
