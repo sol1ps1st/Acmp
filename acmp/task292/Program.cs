71dd7b4b8b8e28a318ac5ba83b44901717ed93b9
Alex <alvi88@list.ru>
2016-05-30 10:15:24 +0300

    add 292 task

---
+++ acmp/task292/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace task292
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            uint n = uint.Parse(sr.ReadLine());
+            sr.Close();
+            uint r = SimpleRoot(n);
+            StreamWriter sw = new StreamWriter("output.txt");
+            sw.Write(r);
+            sw.Close();
+        }
+
+        private static uint SimpleRoot(uint n)
+        {
+            if (IsSimple(n)) return n;
+            if (n < 10) return 0;
+            uint sum = 0; uint buf;
+            while (n > 0)
+            {
+                buf = n / 10;
+                sum += n - buf * 10;
+                n = buf;
+            }
+            return SimpleRoot(sum);
+        }
+
+        private static bool IsSimple(uint n)
+        {
+            if (n == 1) return false;
+            if (n == 2) return true;
+            if (n % 2 == 0) return false;
+            uint max = (uint)Math.Ceiling(Math.Sqrt(n));
+            for (uint i = 3; i <= max; i+=2)
+                if (n % i == 0) return false;
+            return true;
+        }
+    }
+}
