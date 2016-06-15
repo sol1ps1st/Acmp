023c264c7c9c8683b61cdb454dadf502ded7fa62
Alex <alvi88@list.ru>
2016-06-15 10:46:43 +0300

    add 50 task

---
+++ acmp/task50/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace task50
+{
+    class Program
+    {
+        private static string str;
+        private static string subStr;
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            str = sr.ReadLine();
+            subStr = sr.ReadLine();
+            sr.Close();
+            StreamWriter sw = new StreamWriter("output.txt");
+            sw.Write(GetSubCount());
+            sw.Close();
+        }
+
+        static int GetSubCount()
+        {
+            int cnt = 0, i, subStrLen = subStr.Length, pos = 0;
+            List<string> shiftsubStr = new List<string>(subStrLen);
+            string buf;
+            for (i = 0; i < subStrLen; ++i)
+            {
+                buf = subStr.Substring(i) + subStr.Substring(0, i);
+                if (shiftsubStr.IndexOf(buf) != -1) continue;
+                shiftsubStr.Add(buf);
+                pos = 0;
+                while ((pos = str.IndexOf(buf, pos)) != -1)
+                {
+                    cnt++;
+                    pos++;
+                }
+            }
+            return cnt;
+        }
+    }
+}
