3091c6e16b149bece62deb941355ba58ed9264f1
Alex <alvi88@list.ru>
2016-06-22 21:06:04 +0300

    add 269

---
+++ acmp/task269/Break.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task269
+{
+    class Break
+    {
+        private int[] line;
+        private int lineLen;
+        public int this[int i]
+        {
+            get
+            {
+                return line[i];
+            }
+        }
+        public int Count()
+        {
+            return lineLen;
+        }
+        public Break(string line)
+        {
+            lineLen = line.Length;
+            this.line = new int[lineLen];
+            for (int i = 0; i < lineLen; ++i)
+                this.line[i] = int.Parse(line.Substring(i, 1));
+        }
+    }
+}
---
+++ acmp/task269/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace task269
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            Break f = new Break(sr.ReadLine());
+            Break s = new Break(sr.ReadLine());
+            sr.Close();
+            StreamWriter sw = new StreamWriter("output.txt");
+            sw.Write(GetMinBreakLen(f,s));
+            sw.Close();
+        }
+
+        private static int GetMinBreakLen(Break f, Break s)
+        {
+            //пробуем смещать первую пластину относительно второй
+            int shift, res = f.Count() + s.Count(), tmp = 0;
+            for (shift = 0; shift < s.Count() - 1; ++shift)
+            {
+                if (IsMerge(f, shift, s, ref tmp))
+                    res = res < tmp ? res : tmp;
+            }
+            //пробуем смещать вторую пластину относительно первой
+            for (shift = 0; shift < f.Count() - 1; ++shift)
+            {
+                if (IsMerge(s, shift, f, ref tmp))
+                    res = res < tmp ? res : tmp;
+            }
+
+            return res;
+        }
+
+        private static bool IsMerge(Break f, int fShift, Break s, ref int len)
+        {
+            int maxIdx = Math.Min(s.Count() - fShift, f.Count());
+            for(int i = 0; i < maxIdx; ++i)
+            {
+                if (f[i] + s[i+ fShift] > 3) return false;
+            }
+            len = Math.Max(s.Count(), f.Count() + fShift);
+            return true;
+        }
+
+    }
+}
