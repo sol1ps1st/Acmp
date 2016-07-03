2fecb82eadfb96354765b6deebb576c9e0944c5e
Alex <alvi88@list.ru>
2016-07-03 14:25:32 +0300

    add 626

---
+++ acmp/task626/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace task626
+{
+    class Program
+    {
+        private static int n;
+        private static List<string> rules;
+        private static string str;
+
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            n = int.Parse(sr.ReadLine());
+            rules = new List<string>(n);
+            for(int i = 0; i < n; ++i)
+                rules.Add(sr.ReadLine());
+            str = sr.ReadLine();
+            sr.Close();
+            StreamWriter sw = new StreamWriter("output.txt");
+            sw.Write(TransformStr());
+            sw.Close();
+        }
+
+        private static string TransformStr()
+        {
+            int i, pos;
+            int cnt;
+            do
+            {
+                cnt = 0;
+                for (i = 0; i < rules.Count; ++i)
+                {
+                    pos = str.IndexOf(rules[i]);
+                    if (pos != -1)
+                    {
+                        str = str.Remove(pos, rules[i].Length);
+                        ++cnt;
+                    }
+                }
+            } while (cnt > 0);
+            return str;
+        }
+    }
+}
