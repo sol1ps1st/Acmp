0620baca09354f2aaface247c2395dc8bc259945
Alex <alvi88@list.ru>
2016-12-08 10:44:48 +0300

    Add 39 task

---
+++ acmp/task39/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace task39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("INPUT.TXT");
+            StreamWriter sw = new StreamWriter("OUTPUT.TXT");
+            int n = Int32.Parse(sr.ReadLine());
+            int[] c = new int[n];
+            string[] s = sr.ReadLine().Split();
+            sr.Close();
+            for (int i = 0; i < n; ++i)
+                c[i] = Int32.Parse(s[i]);
+
+            int max, pos = 0, maxPos, sum = 0, hairLen = 1;
+            while (pos < n)
+            {
+                max = c[pos]; maxPos = pos;
+                for (int i = pos + 1; i < n; ++i)
+                    if (c[i] > max) { max = c[i]; maxPos = i; }
+                hairLen += maxPos - pos;
+                sum += max * hairLen;
+                pos = maxPos + 1; hairLen = 1;
+            }
+            sw.Write(sum);
+            sw.Close();
+        }
+    }
+}
