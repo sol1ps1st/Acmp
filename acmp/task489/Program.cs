1ccf2285fecd5e8bc2cec4bd6ce27676097f6372
Альберг Александр Викторович <albergav@oducn.so>
2017-01-23 16:12:36 +0300

    489

--- acmp/task489/Program.cs
+++ acmp/task489/Program.cs
@@ -39,13 +39,37 @@ namespace task489
                 return Math.Abs(x).CompareTo(Math.Abs(y));
             };
             begCoins.Sort(AbsSort); endCoins.Sort(AbsSort);
-            int i, с = Math.Abs(begCoins[n-1]);
-            for (i = 0; i < n-2; ++i) //В endCoins на одну монету меньше. Ее значение и надо найти
+            int i, c = Math.Abs(begCoins[n-1]);
+            for (i = 0; i < n-1; ++i) //В endCoins на одну монету меньше. Ее значение и надо найти
             {
                 if (Math.Abs(begCoins[i]) != Math.Abs(endCoins[i]))
-                { с = Math.Abs(begCoins[i]); break; }
+                { c = Math.Abs(begCoins[i]); break; }
             }
-            return с;
+            begCoins = begCoins.FindAll(delegate(int x) { return (x == c) || (x == -c); });
+            endCoins = endCoins.FindAll(delegate(int x) { return (x == c) || (x == -c); });
+            rotations = rotations.FindAll(delegate(int x) { return (x == c) || (x == -c); });
+
+            int bSum = 0, eSum = 0, rSum = 0;
+            foreach (int e in begCoins)
+                bSum += e > 0 ? 1 : -1;
+            foreach (int e in endCoins)
+                eSum += e > 0 ? 1 : -1;
+            foreach (int e in rotations)
+                rSum ++;
+
+            if (rSum % 2 == 0)
+            {
+                //Четное количеcтво вращений целевых монет (cтоимоcтью c) Они не изменят иcходный набор монет - изменитcя может только порядок cледования монет.
+                return (bSum - eSum) * c;
+            }
+            else
+            {
+                //Считаем, что сняли плюсовую монету, тогда должно выполниться условие (Поскольку за один шаг кол-во плюсовых монет может измениться только на 2)
+                if (Math.Abs(eSum + 1 - bSum) == 2)
+                    return c; 
+                else
+                    return -c;
+            }
         }
     }
 }
